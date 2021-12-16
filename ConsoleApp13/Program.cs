10bbefdc5a623b4e09f1c577f5bad66fd4d7a5b6
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-16 13:46:12 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y:");
+            double y = double.Parse(Console.ReadLine());
+            if (y >= x+1 && y <= x+2 && y <= x - 1 && y >= x - 2) Console.WriteLine("Точка попадает в область");
+            else Console.WriteLine("Точка не попадает в область");
+        }
+    }
+}
